8f62611ba99e131b7637618f47f0b58ff92079d4
MAPSRafaelH <rafael.haddad@maps.com.br>
2023-02-09 14:40:02 -0300

    First Commit

---
+++ maps/Background-Pricing-Meet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Background-Pricing-Meet" tilewidth="2560" tileheight="1440" tilecount="1" columns="1">
+ <image source="../../../../Desktop/MAPS Onboarding/Áreas Maps/Background-Pricing-Meet.png" width="2560" height="1440"/>
+</tileset>
---
+++ maps/Captura de tela_20230208_175924.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Captura de tela_20230208_175924" tilewidth="588" tileheight="185" tilecount="1" columns="1">
+ <image source="../../../../Pictures/Screenshots/Captura de tela_20230208_175924.png" width="588" height="185"/>
+</tileset>
